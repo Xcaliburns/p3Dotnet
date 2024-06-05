a723261e032b8728f62c2dac3deb1a8d4b617735
davidabruzzogames <davidabruzzogames@gmail.com>
2024-06-05 09:15:16 +0200

    validation conditions applied

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
 {
@@ -7,14 +9,21 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
         [BindNever]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "MissingName")]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Details { get; set; }
 
+        [Required(ErrorMessage = "MissingQuantity")]
+        [Range(1, int.MaxValue, ErrorMessage = "QuantityNotGreaterThanZero")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "QuantityNotAnInteger")]
         public string Stock { get; set; }
 
+        [Required(ErrorMessage = "MissingPrice")]
+        [Range(1, int.MaxValue, ErrorMessage = "PriceNotGreaterThanZero")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "PriceNotANumber")]
         public string Price { get; set; }
     }
 }
